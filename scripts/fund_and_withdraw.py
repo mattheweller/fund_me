f3de272fe1b3969c06f94db1adec985bb9e5c6db
mattheweller <mattcjeller@gmail.com>
2021-11-16 11:16:17 +0000

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,25 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    print(f"The current account is {account}")
+    fund_me.withdraw({"from": account})
+    print(f"The withdraw is {entrance_fee}")
+
+
+def main():
+    fund()
+    withdraw()
